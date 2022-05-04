fa14d53914899c77a9f96c13c0eb0ff7081e1eff
Dangbok <ghdwldnjs99@naver.com>
2022-05-05 03:58:14 +0900

    수정1

--- app/src/main/java/com/example/login5/RegisterActivity.java
+++ app/src/main/java/com/example/login5/RegisterActivity.java
@@ -1,52 +1,33 @@
 package com.example.login5;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.content.CursorLoader;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    //회원가입 입력필드(이메일,비밀번호,이름,나이,mbti,자기소개)
-    private EditText et_id, et_password, et_name, et_mbti;
-    private ImageView iv_iv;
+    private static final String TAG = "RegisterActivity";
+    EditText et_email, et_pass, et_name,et_age, et_mbti,et_myself;
+    Button btn_register1;
+    private FirebaseAuth firebaseAuth;
 
-    // 버튼필드(사진 등록, 회원가입, 뒤로가기)
-    private Button btn_im, btn_register, btn_back;
-
-    // 파이어베이스 처리
-    private FirebaseAuth mFirebaseAuth; //파이어베이스 인증처리
-    private FirebaseDatabase firebaseDatabase;
-    private FirebaseStorage storage;
-    private DatabaseReference mDatabaseRef; //실시간 데이터베이스 연동 객체
-
-    private DBUser dbUser;
 
     // 액티비티 시작시 처음으로 실행되는 생명주기
     @Override
@@ -54,85 +35,177 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        et_id = findViewById(R.id.et_id); //아이디
-        et_password = findViewById(R.id.et_password); //비밀번호
-        et_name = findViewById(R.id.et_name); //이름
-        et_mbti = findViewById(R.id.et_mbti); //mbti
-        btn_im = findViewById(R.id.btn_im); //사진 등록하기 버튼
-        btn_register = findViewById(R.id.btn_register); //회원가입 버튼
-        btn_back = findViewById(R.id.btn_back); //뒤로가기 버튼
+        //액션 바 등록하기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("회원가입");
 
-        //파이어베이스 이용
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        storage = FirebaseStorage.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("login");
+        actionBar.setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
+        actionBar.setDisplayShowHomeEnabled(true); //홈 아이콘
 
-        // 모든 정보 입력 후 회원가입 버튼 클릭 시 수행
-        btn_register.setOnClickListener(new View.OnClickListener() {
+        //파이어베이스 접근 설정
+        // user = firebaseAuth.getCurrentUser();
+        firebaseAuth = FirebaseAuth.getInstance();
+        //firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+
+        et_email = findViewById(R.id.et_email);
+        et_pass = findViewById(R.id.et_pass);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        et_mbti = findViewById(R.id.et_mbti);
+        et_myself = findViewById(R.id.et_myself);
+        btn_register1=findViewById(R.id.btn_register1);
+
+
+        //가입버튼 클릭리스너   -->  firebase에 데이터를 저장한다.
+        btn_register1.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
 
-                // EditText에 현재 입력되어 있는 값을 get(가져온다)해온다.
-                String userID = et_id.getText().toString().trim();
-                String userPass = et_password.getText().toString().trim();
-                String userName = et_name.getText().toString().trim();
-                String userMbti = et_mbti.getText().toString().trim();
-//                String userMyself = et_myself.getText().toString().trim();
+                //가입 정보 가져오기
+                final String email = et_email.getText().toString().trim();
+                String pass = et_pass.getText().toString().trim();
+                String name = et_name.getText().toString().trim();
+                String age = et_age.getText().toString().trim();
+                String mbti = et_mbti.getText().toString().trim();
+                String myself = et_myself.getText().toString().trim();
 
-                UserAccount userAccount = new UserAccount(String userID, String userPass, String userName, String userMbti);
 
-                //Firebase Auth 진행
-                mFirebaseAuth.createUserWithEmailAndPassword(userID, userPass).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                //파이어베이스에 신규계정 등록하기
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        //가입 성공시
                         if (task.isSuccessful()) {
-                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                            UserAccount account = new UserAccount("", userID, userPass, userName, userMbti);
-                            account.setIdToken(firebaseUser.getUid());
-                            account.setEmailId(firebaseUser.getEmail());
-                            account.setPassword(userPass);
-                            account.setName(firebaseUser.get);
-                            account.setMbti(firebaseUser.);
 
-                            // setValue : 데이터베이스에 insert(삽입) 행위
-                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+                            String pass = et_pass.getText().toString().trim();
+                            String name = et_name.getText().toString().trim();
+                            String age = et_age.getText().toString().trim();
+                            String mbti = et_mbti.getText().toString().trim();
+                            String myself = et_myself.getText().toString().trim();
 
-                            public void onDataChage (@NonNull DataSnapshot dataSnapshot){
-                                UserAccount group = dataSnapshot.getValue(UserAccount.class);
+                            //해쉬맵 테이블을 파이어베이스 데이터베이스에 저장
+                            HashMap<Object, String> hashMap = new HashMap<>();
 
-                                name = group.getName();
-                                age = group.getAge();
-                                userMbti[0] = group.getName();
-                                userMyself[0] = group.getName();
+                            hashMap.put("uid", uid);
+                            hashMap.put("email", email);
+                            hashMap.put("password", pass);
+                            hashMap.put("name", name);
+                            hashMap.put("age", age);
+                            hashMap.put("MBTI", mbti);
+                            hashMap.put("myself", myself);
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference reference = database.getReference("Users");
+                            reference.child(uid).setValue(hashMap);
 
 
-                            }
+                            //가입이 이루어져을시 가입 화면을 빠져나감.
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
                             Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+
                         } else {
-                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegisterActivity.this, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                            return;  //해당 메소드 진행을 멈추고 빠져나감.
+
                         }
-
-                        // 사진 등록하기 버튼 클릭시 이동
-                        btn_im.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Intent intent = new Intent(RegisterActivity.this, ImageActivity.class);
-                                startActivity(intent);  //액티비티 이동
-                            }
-                        });
-
-                        // 뒤로가기 버튼 클릭시 이동
-                        btn_back.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                startActivity(intent);  //액티비티 이동
-                            }
-                        });
                     }
                 });
             }
         });
     }
+
+    public boolean onSupportNavigateUp(){
+        onBackPressed();; // 뒤로가기 버튼이 눌렸을시
+        return super.onSupportNavigateUp(); // 뒤로가기 버튼
+    }
 }
+
+
+//        et_id = findViewById(R.id.et_id); //아이디
+//        et_password = findViewById(R.id.et_password); //비밀번호
+//        et_name = findViewById(R.id.et_name); //이름
+//        et_mbti = findViewById(R.id.et_mbti); //mbti
+//        btn_im = findViewById(R.id.btn_im); //사진 등록하기 버튼
+//        btn_register = findViewById(R.id.btn_register); //회원가입 버튼
+//        btn_back = findViewById(R.id.btn_back); //뒤로가기 버튼
+//
+//        //파이어베이스 이용
+//        mFirebaseAuth = FirebaseAuth.getInstance();
+//        storage = FirebaseStorage.getInstance();
+//        firebaseDatabase = FirebaseDatabase.getInstance();
+//        mDatabaseRef = FirebaseDatabase.getInstance().getReference("login");
+//
+//        // 모든 정보 입력 후 회원가입 버튼 클릭 시 수행
+//        btn_register.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                // EditText에 현재 입력되어 있는 값을 get(가져온다)해온다.
+//                String userID = et_id.getText().toString().trim();
+//                String userPass = et_password.getText().toString().trim();
+//                String userName = et_name.getText().toString().trim();
+//                String userMbti = et_mbti.getText().toString().trim();
+////                String userMyself = et_myself.getText().toString().trim();
+//
+//                UserAccount userAccount = new UserAccount(String userID, String userPass, String userName, String userMbti);
+//
+//                //Firebase Auth 진행
+//                mFirebaseAuth.createUserWithEmailAndPassword(userID, userPass).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+//                            UserAccount account = new UserAccount("", userID, userPass, userName, userMbti);
+//                            account.setIdToken(firebaseUser.getUid());
+//                            account.setEmailId(firebaseUser.getEmail());
+//                            account.setPassword(userPass);
+//                            account.setName(firebaseUser.get);
+//                            account.setMbti(firebaseUser.);
+//
+//                            // setValue : 데이터베이스에 insert(삽입) 행위
+//                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+//
+//                            public void onDataChage (@NonNull DataSnapshot dataSnapshot){
+//                                UserAccount group = dataSnapshot.getValue(UserAccount.class);
+//
+//                                name = group.getName();
+//                                age = group.getAge();
+//                                userMbti[0] = group.getName();
+//                                userMyself[0] = group.getName();
+//
+//
+//                            }
+//                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+//                        } else {
+//                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+//                        }
+//
+//                        // 사진 등록하기 버튼 클릭시 이동
+//                        btn_im.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Intent intent = new Intent(RegisterActivity.this, ImageActivity.class);
+//                                startActivity(intent);  //액티비티 이동
+//                            }
+//                        });
+//
+//                        // 뒤로가기 버튼 클릭시 이동
+//                        btn_back.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+//                                startActivity(intent);  //액티비티 이동
+//                            }
+//                        });
+//                    }
+//                });
+//            }
+//        });
+//    }
