b2f59a15544a6608774da0a5afe0cd6e3a7547a6
Dangbok <101463032+Dangbok@users.noreply.github.com>
2022-05-04 17:38:07 +0900

    Add files via upload

---
+++ DBUser.java
@@ -0,0 +1,24 @@
+package com.example.login5;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DBUser {
+
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase;
+    private Object UserAccount;
+
+    DBUser(){
+        FirebaseDatabase db=FirebaseDatabase.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=db.getReference(UserAccount.class.getSimpleName());
+    }
+
+    //등록
+    public Task<Void> add(UserAccount userAccount){
+        return databaseReference.push().setValue(UserAccount);
+    }
+
+}
